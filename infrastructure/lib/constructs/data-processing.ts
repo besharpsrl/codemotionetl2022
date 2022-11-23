a3dfc29b40cd91ddaadab9666af11799add59562
Matteo Goretti <matteo.goretti@besharp.it>
2022-11-23 22:44:00 +0100

    fix networking glue

--- infrastructure/lib/constructs/data-processing.ts
+++ infrastructure/lib/constructs/data-processing.ts
@@ -78,6 +78,7 @@ export class DataProcessing extends Construct {
             vpc: props.vpc,
             allowAllOutbound: true
         });
+        jobSG.addIngressRule(Peer.securityGroupId(jobSG.securityGroupId), Port.allTcp(), 'Self ingress')
         props.redshiftSG.addIngressRule(Peer.securityGroupId(jobSG.securityGroupId), Port.tcp(5439), 'Ingress from Glue Job')
 
         const jobLogGroup = new LogGroup(this, `JobLogGroup`, {
@@ -108,7 +109,7 @@ export class DataProcessing extends Construct {
                 "KAFKA_SSL_ENABLED": "false"
             },
             securityGroups: [jobSG],
-            subnet: props.subnets.private[2],
+            subnet: props.subnets.natted[2],
         })
 
     
