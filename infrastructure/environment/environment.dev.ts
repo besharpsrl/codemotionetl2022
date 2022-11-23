e41e7039591d1c83a260d3e0f9d87ea64cdf4a56
Matteo Goretti <matteo.goretti@besharp.it>
2022-11-23 23:56:14 +0100

    test az

--- infrastructure/environment/environment.dev.ts
+++ infrastructure/environment/environment.dev.ts
@@ -10,19 +10,19 @@ export const environment = {
     vpcId: 'vpc-0daaee1d7c181cf2b',
     subnets: {
         public: [
-            { subnetId: 'subnet-08947c72435faa085', availabilityZone: 'euw1-az1' },
-            { subnetId: 'subnet-014ae6ee9b761097f', availabilityZone: 'euw1-az2' },
-            { subnetId: 'subnet-079110212b62bf33a', availabilityZone: 'euw1-az3' }
+            { subnetId: 'subnet-08947c72435faa085', availabilityZone: 'eu-west-1a' },
+            { subnetId: 'subnet-014ae6ee9b761097f', availabilityZone: 'eu-west-1b' },
+            { subnetId: 'subnet-079110212b62bf33a', availabilityZone: 'eu-west-1c' }
         ],
         private: [
-            { subnetId: 'subnet-05655e10d4bd92d95', availabilityZone: 'euw1-az1' },
-            { subnetId: 'subnet-08b377a9b93cc94cb', availabilityZone: 'euw1-az2' },
-            { subnetId: 'subnet-0868c3822a1f45b0b', availabilityZone: 'euw1-az3' }
+            { subnetId: 'subnet-05655e10d4bd92d95', availabilityZone: 'eu-west-1a' },
+            { subnetId: 'subnet-08b377a9b93cc94cb', availabilityZone: 'eu-west-1b' },
+            { subnetId: 'subnet-0868c3822a1f45b0b', availabilityZone: 'eu-west-1c' }
         ],
         natted: [
-            { subnetId: 'subnet-043cd7bcac84f76c6', availabilityZone: 'euw1-az1' },
-            { subnetId: 'subnet-09e442c1c82672557', availabilityZone: 'euw1-az2' },
-            { subnetId: 'subnet-06c5bbe5752b87207', availabilityZone: 'euw1-az3' }
+            { subnetId: 'subnet-043cd7bcac84f76c6', availabilityZone: 'eu-west-1a' },
+            { subnetId: 'subnet-09e442c1c82672557', availabilityZone: 'eu-west-1b' },
+            { subnetId: 'subnet-06c5bbe5752b87207', availabilityZone: 'eu-west-1c' }
         ],
     },
 };
